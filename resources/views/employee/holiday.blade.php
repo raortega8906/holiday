ba60e5cbec2abcc1c52bdacbba6631d1c4b2adbb
Rafael <raortega8906@gmail.com>
2021-03-30 01:44:20 +0200

    Proyecto Terminado v1.1.6

--- resources/views/employee/holiday.blade.php
+++ resources/views/employee/holiday.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Sistema Vacaciones | Holidays</title>
+
     <!-- Tell the browser to be responsive to screen width -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Font Awesome -->
