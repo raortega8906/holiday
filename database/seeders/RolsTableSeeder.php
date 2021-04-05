bad4cb073d167b7c6085f12e653fd2f22ab4b478
Rafael <raortega8906@gmail.com>
2021-04-05 22:16:24 +0200

    Deploy Heroku Holidays DB

---
+++ database/seeders/RolsTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Rol;
+
+class RolsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Rol::create([
+        	'id' => 1,
+            'key' => 'admin',
+            'name' => 'Rol Admin',
+            'description' => 'Todos los permisos'
+        ]);
+        Rol::create([
+        	'id' => 2,
+            'key' => 'employee',
+            'name' => 'Rol Employee',
+            'description' => 'Permisos de consulta y solicitud'
+        ]);
+    }
+}
