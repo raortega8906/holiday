1aaa0498794435dc34e3ae0409ab71cb2cabea81
Rafael <raortega8906@gmail.com>
2021-04-06 02:49:51 +0200

    Deploy Heroku Holidays DB

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -26,4 +26,15 @@ class UsersTableSeeder extends Seeder
 
         User::create($user1);
     }
+
+    // Random generate ID
+    function generateTokenID($length = 9) {
+        $characters = '123456789';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomString;
+    }
 }
