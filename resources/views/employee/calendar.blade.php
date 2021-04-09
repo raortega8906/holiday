3d0eabff137d62af752329acb95962fdc408ca5d
Rafael <raortega8906@gmail.com>
2021-04-09 19:08:27 +0200

    Deploy Holidays Heroku

--- resources/views/employee/calendar.blade.php
+++ resources/views/employee/calendar.blade.php
@@ -121,7 +121,7 @@
             <div class="container-fluid">
                 <div class="row mb-2">
                     <div class="col-sm-6">
-                        <h1>Calendar</h1>
+                        <h1>Calendario</h1>
                     </div>
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
