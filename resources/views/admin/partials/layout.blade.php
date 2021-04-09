f423c877bc01fc77ece28214b0d097d6fdfdbe79
Rafael <raortega8906@gmail.com>
2021-04-09 18:48:25 +0200

    Deploy Holidays Heroku

--- resources/views/admin/partials/layout.blade.php
+++ resources/views/admin/partials/layout.blade.php
@@ -34,7 +34,10 @@ to get the desired effect
                 <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
             </li>
             <li class="nav-item d-none d-sm-inline-block">
-                <a href="{{url('/admin/calendar')}}" class="nav-link">Home</a>
+                <a href="{{ url('/admin/calendar') }}" class="nav-link">Home</a>
+            </li>
+            <li class="nav-item d-none d-sm-inline-block">
+                <a href="{{ url('/admin') }}" class="nav-link">Admin</a>
             </li>
         </ul>
     </nav>
