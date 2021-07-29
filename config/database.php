4212675e0f684a86d49772f9d782896edea627d3
Rafael <raortega8906@gmail.com>
2021-07-30 01:37:07 +0200

    finished project

--- config/database.php
+++ config/database.php
@@ -71,20 +71,20 @@ return [
         ],
 
         // HEROKU: Deshabilitar para desarrollo.
-        'mysql' => [
-            'driver' => 'mysql',
-            'host' => $host,
-            'port' => env('DB_PORT', '3306'),
-            'database' => $database,
-            'username' => $username,
-            'password' => $password,
-            'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => '',
-            'strict' => true,
-            'engine' => null,
-        ],
+        // 'mysql' => [
+        //     'driver' => 'mysql',
+        //     'host' => $host,
+        //     'port' => env('DB_PORT', '3306'),
+        //     'database' => $database,
+        //     'username' => $username,
+        //     'password' => $password,
+        //     'unix_socket' => env('DB_SOCKET', ''),
+        //     'charset' => 'utf8mb4',
+        //     'collation' => 'utf8mb4_unicode_ci',
+        //     'prefix' => '',
+        //     'strict' => true,
+        //     'engine' => null,
+        // ],
 
         'pgsql' => [
             'driver' => 'pgsql',
