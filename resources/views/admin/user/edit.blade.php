61dd3e7561fbf61b88300cb424b3c3fb6fab3ade
Rafael <raortega8906@gmail.com>
2021-07-31 00:24:58 +0200

    fixed user permissions on views

--- resources/views/admin/user/edit.blade.php
+++ resources/views/admin/user/edit.blade.php
@@ -46,18 +46,6 @@
                                     @method('PUT')
                                     @csrf
                                     <div class="form-group mb-3 mt-3 ml-3 row-cols-lg-3">
-                                        <label for="name">Empleado:</label>
-                                        <input type="text" class="form-control" name="name" id="name" value="{{ old('title', $user->name) }}" disabled>
-                                    </div>
-                                    <div class="form-group mb-3 mt-3 ml-3 row-cols-lg-3">
-                                        <label for="email">Correo electrónico:</label>
-                                        <input type="email" class="form-control" name="email" id="email" value="{{ old('title', $user->email) }}" disabled>
-                                    </div>
-                                    <div class="form-group mb-3 mt-3 ml-3 row-cols-lg-3">
-                                        <label for="password">Contraseña:</label>
-                                        <input type="text" class="form-control" name="password" id="password" value="{{ old('title', '********') }}" disabled>
-                                    </div>
-                                    <div class="form-group mb-3 mt-3 ml-3 row-cols-lg-3">
                                         <label for="is_admin">Rol del empleado</label>
                                         <select class="form-control" name="is_admin" id="is_admin">
                                             <option value="0">Empleado</option>
